4684405aa5be451f8043e708d83ccbc49794e171
Udi Levy <udisapp@gmail.com>
2020-12-02 22:01:35 +0200

    remove no need console

--- app-template/src/index.ts
+++ app-template/src/index.ts
@@ -14,5 +14,4 @@ app.listen(app.get('port'), () => {
   const address = format('http://localhost:%d', app.get('port'));
   console.log('  App is running at %s in %s mode', address, app.get('env'));
   console.log('  Press CTRL-C to stop\n');
-  console.log('  Making HTTP calls..');
 });
